00b9502da57297183edb36280418671b5c3de017
moonsung1234 <dit67890@gmail.com>
2023-10-09 17:48:53 +0900

    sitemap added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let express = require("express");
 let app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "view")));
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
